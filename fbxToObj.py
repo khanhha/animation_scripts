da2821ad56dd1f12a14b48fc07a519138cab8fca
khanhha <khanhhh89@gmail.com>
2019-02-25 09:59:20 +0100

    add frame export option. add more assertion, checking

--- fbxToObj.py
+++ fbxToObj.py
@@ -43,6 +43,15 @@ from pathlib import Path
 
 G_debug = False
 
+def select_single_obj(target_obj):
+    for obj in bpy.data.objects:
+        obj.select = False
+    target_obj.select = True
+    for c in target_obj.children:
+        c.select = True
+    bpy.context.scene.objects.active = target_obj
+    
+    
 def Usage():
     print("Usage: blender --background --python fbxToObj.py -- <fbx input> {-add_rest} <output folder>")
     sys.exit(0)
@@ -51,16 +60,18 @@ if not G_debug:
     ap = argparse.ArgumentParser()
     ap.add_argument("--background",  required=True, action='store_true', help="dummy argument to get around blender")
     ap.add_argument("--python",  required=True, type=str, help="dummy argument to get around blender")
-    ap.add_argument("-fbx_file", required=True, type=str, help="input fbx file")
+    ap.add_argument("--fbx_file", required=True, type=str, help="input fbx file")
     ap.add_argument("--add_rest",  required=False, action='store_true', help="add this option to insert rest pose and export fbx")
     ap.add_argument("--out_rest_fbx_file", required=False, type=str, default='', help='if add_rest is on, this parameter must specify output fbx path')
     ap.add_argument("--out_obj_dir", required=False, type=str, default='', help='if this option is available, obj file corresponding to each frame will be exported')
+    ap.add_argument("--frame_idx", required=False, type=int, default=0, help='frame index to be exported. if it is negative, all frames will be exported')
     args = ap.parse_args()       
 
     add_rest = args.add_rest
     out_rest_fbx_file = args.out_rest_fbx_file
     out_obj_dir = args.out_obj_dir
     fbx_file = args.fbx_file
+    frame_idx = args.frame_idx
 
     if Path(fbx_file).suffix != '.fbx':
         print('input fbx_file does not end with .fbx pattern')
@@ -71,18 +82,18 @@ if not G_debug:
         if not is_valid:
             print('out_rest_fbx_file is not valid. it must be not empty and ends with .fbx pattern')
             sys.exit(0)
-    print('input parameters')
-    print('fbx_file ', fbx_file)
-    print('add_rest ', add_rest)
-    print('out_rest_fbx_file ', out_rest_fbx_file)
-    print('out_obj_dir ', out_obj_dir)
+    print('\ninput parameters')
+    print('\tfbx_file ', fbx_file)
+    print('\tadd_rest ', add_rest)
+    print('\tout_rest_fbx_file ', out_rest_fbx_file)
+    print('\tout_obj_dir ', out_obj_dir)
 else:
     fbx_file = '/home/khanhhh/data_1/projects/Oh/data/animation/Idle.fbx'
     out_obj_dir = '/home/khanhhh/data_1/projects/Oh/data/animation/Idle_output.obj'
     out_obj_dir = ''
     out_rest_fbx_file = '/home/khanhhh/data_1/projects/Oh/data/animation/RestPose.fbx'
     add_rest = True
-    
+    frame_idx = 0
 
 #
 # clear scene
@@ -92,21 +103,34 @@ bpy.ops.scene.delete()
 #
 # import FBX
 #
-print("loading file: " + fbx_file)
+print("\nloading file: " + fbx_file)
 bpy.ops.import_scene.fbx(filepath = fbx_file)
 
 
+armature_obj = None
+for obj in bpy.data.objects:
+    if isinstance(obj.data, bpy.types.Armature):
+        armature_obj = obj
+        print('\nfound one Armature object in the fbx file. its name is: ', armature_obj.name)
+        break
+
+if armature_obj is None:
+    print('\nobj of type Armature is not found in the scene')
+    sys.exit(0)    
+    
 #
 # add pose as first frame
 #
 if (add_rest):
+    print('\ninserted the rest frame at frame index 0')
     bpy.ops.object.mode_set(mode='POSE')
-    bpy.data.scenes['Scene'].frame_current=0
+    bpy.data.scenes['Scene'].frame_set(0)
     bpy.ops.pose.transforms_clear()
-    obj = bpy.context.object
-    for bone in obj.pose.bones:
+    for bone in armature_obj.pose.bones:
         bone.keyframe_insert("rotation_quaternion",frame=0)
-    bpy.ops.export_scene.fbx(filepath=out_rest_fbx_file)
+        
+    select_single_obj(armature_obj)
+    bpy.ops.export_scene.fbx(filepath=out_rest_fbx_file, use_selection=True)
 
 #
 #
@@ -116,11 +140,35 @@ if out_obj_dir != '':
     
     os.makedirs(out_obj_dir, exist_ok = True)
     
-    out_name = Path(fbx_file).stem + '.obj'
-    print("Outputting with prefix: " + out_name)
-    out_path = os.path.join(*[out_obj_dir, out_name])
+    if frame_idx  < 0:
+        out_name = Path(fbx_file).stem + '.obj'
+        print("\nOutputting with prefix: " + out_name)
+        out_path = os.path.join(*[out_obj_dir, out_name])
+        bpy.ops.export_scene.obj(filepath=out_path, check_existing=False, axis_forward='-Z', axis_up='Y', use_selection=True, use_animation=True, use_mesh_modifiers=True, use_edges=True, use_smooth_groups=False, use_smooth_groups_bitflags=False, use_normals=False, use_uvs=False, use_materials=False, use_triangles=True, use_nurbs=False, use_vertex_groups=False, use_blen_objects=True, group_by_object=False, group_by_material=False, keep_vertex_order=True, global_scale=1000.0, path_mode='AUTO')
     
-    bpy.ops.export_scene.obj(filepath=out_path, check_existing=False, axis_forward='-Z', axis_up='Y', use_selection=False, use_animation=True, use_mesh_modifiers=True, use_edges=True, use_smooth_groups=False, use_smooth_groups_bitflags=False, use_normals=False, use_uvs=False, use_materials=False, use_triangles=True, use_nurbs=False, use_vertex_groups=False, use_blen_objects=True, group_by_object=False, group_by_material=False, keep_vertex_order=True, global_scale=1000.0, path_mode='AUTO')
+    else:
+        bpy.data.scenes['Scene'].frame_set(frame_idx)    
+            
+        #find the mesh object under Armature for exporting
+        mesh_obj = None
+        for c in armature_obj.children:
+            if isinstance(c.data, bpy.types.Mesh):
+                print('\nfound one mesh object for exporting under the Armature object. its name is: ', c.name)
+                mesh_obj = c
+        if mesh_obj is None:
+            print('\nmesh obj is not found under Armature. exitting...')
+            sys.exit(0)
+        
+        #just export that mesh obj
+        select_single_obj(mesh_obj)
+        
+        out_name = Path(fbx_file).stem + '_' + str(frame_idx) + '.obj'    
+        print("\nOutputting object: " + out_name)
+        out_path = os.path.join(*[out_obj_dir, out_name])
+        
+        bpy.ops.export_scene.obj(filepath=out_path, check_existing=False, axis_forward='-Z', axis_up='Y', use_selection=True, use_animation=False, use_mesh_modifiers=True, use_edges=True, use_smooth_groups=False, use_smooth_groups_bitflags=False, use_normals=False, use_uvs=False, use_materials=False, use_triangles=True, use_nurbs=False, use_vertex_groups=False, use_blen_objects=True, group_by_object=False, group_by_material=False, keep_vertex_order=True, global_scale=1000.0, path_mode='AUTO')
+                
+
 
 # done
-print("fbxToObj.py completed")
+print("fbxToObj.py completed, please don't mind the later messages by Blender\n\n\n")
